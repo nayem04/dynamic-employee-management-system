bc75295bf9f67efca4b8a2eac6a4d047e1f4f253
Mohammad Nayem <sheikhmdnayem@gmail.com>
2025-01-13 19:41:08 +0600

    Set timezone to Dhaka

--- src/main/java/com/dynamicemployeemanagementsystem/DynamicEmployeeManagementSystemApplication.java
+++ src/main/java/com/dynamicemployeemanagementsystem/DynamicEmployeeManagementSystemApplication.java
@@ -1,13 +1,23 @@
 package com.dynamicemployeemanagementsystem;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.TimeZone;
 
 @SpringBootApplication
 public class DynamicEmployeeManagementSystemApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(DynamicEmployeeManagementSystemApplication.class, args);
+    }
+
+    //@PostConstruct is used to mark a method that should be run after the bean is initialized.
+    @PostConstruct
+    void started() {
+        TimeZone.setDefault(TimeZone.getTimeZone("GMT+06:00"));
+        System.out.println("==== Timezone set to Dhaka ====");
     }
 
 }
