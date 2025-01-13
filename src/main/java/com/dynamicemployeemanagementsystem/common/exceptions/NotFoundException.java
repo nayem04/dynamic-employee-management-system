5653682372c2e1515cc25b0b00b8c9a6a5591f26
Mohammad Nayem <sheikhmdnayem@gmail.com>
2025-01-13 20:07:00 +0600

    Added exception class NotFoundException

---
+++ src/main/java/com/dynamicemployeemanagementsystem/common/exceptions/NotFoundException.java
@@ -0,0 +1,10 @@
+package com.dynamicemployeemanagementsystem.common.exceptions;
+
+public class NotFoundException extends Exception {
+    public NotFoundException() {
+    }
+
+    public NotFoundException(String message) {
+        super(message);
+    }
+}
