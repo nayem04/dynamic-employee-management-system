ee29e17f4178009853c3ee2b0082021b493b40b0
Mohammad Nayem <sheikhmdnayem@gmail.com>
2025-01-13 20:04:39 +0600

    Added constant Msg class for provide response message

---
+++ src/main/java/com/dynamicemployeemanagementsystem/common/constants/Msg.java
@@ -0,0 +1,14 @@
+package com.dynamicemployeemanagementsystem.common.constants;
+
+public final class Msg {
+    public final static class Entity {
+    }
+
+    public final static class Response {
+        public final static String SEARCH = " Searched Successfully.";
+        public final static String FIND = " Found Successfully.";
+        public final static String CREATE = " Created Successfully.";
+        public final static String UPDATE = " Updated Successfully.";
+        public final static String DELETE = " Deleted Successfully.";
+    }
+}
