b569ff127b03c1a23dba8101c0aeab9a16a1844d
Mohammad Nayem <sheikhmdnayem@gmail.com>
2025-01-14 20:13:29 +0600

    Added NullException class

---
+++ src/main/java/com/dynamicemployeemanagementsystem/common/exceptions/NullException.java
@@ -0,0 +1,10 @@
+package com.dynamicemployeemanagementsystem.common.exceptions;
+
+public class NullException extends Exception {
+    public NullException() {
+    }
+
+    public NullException(String message) {
+        super(message);
+    }
+}
