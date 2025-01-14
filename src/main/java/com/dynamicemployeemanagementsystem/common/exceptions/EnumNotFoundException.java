e1d10b65c6ce2eee25e373ea594088705dc8f822
Mohammad Nayem <sheikhmdnayem@gmail.com>
2025-01-14 20:19:37 +0600

    Added EnumNotFoundException class

---
+++ src/main/java/com/dynamicemployeemanagementsystem/common/exceptions/EnumNotFoundException.java
@@ -0,0 +1,10 @@
+package com.dynamicemployeemanagementsystem.common.exceptions;
+
+public class EnumNotFoundException extends NotFoundException {
+    public EnumNotFoundException() {
+    }
+
+    public EnumNotFoundException(String message) {
+        super(message);
+    }
+}
