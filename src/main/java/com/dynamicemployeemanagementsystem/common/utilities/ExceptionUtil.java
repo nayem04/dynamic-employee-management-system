71087aa8c8791117f58519e23268031efd89d9bc
Mohammad Nayem <sheikhmdnayem@gmail.com>
2025-01-14 20:25:13 +0600

    Added methods to the ExceptionUtil class to generate exceptions

--- src/main/java/com/dynamicemployeemanagementsystem/common/utilities/ExceptionUtil.java
+++ src/main/java/com/dynamicemployeemanagementsystem/common/utilities/ExceptionUtil.java
@@ -1,6 +1,6 @@
 package com.dynamicemployeemanagementsystem.common.utilities;
 
-import com.dynamicemployeemanagementsystem.common.exceptions.NotFoundException;
+import com.dynamicemployeemanagementsystem.common.exceptions.*;
 
 public final class ExceptionUtil {
     public static NotFoundException getNotFoundException(String name, Long id) {
@@ -9,5 +9,29 @@ public final class ExceptionUtil {
 
     public static NotFoundException getNotFoundException(String name, String code) {
         return new NotFoundException("Could not find '" + name + "' by : " + code);
+    }
+
+    public static EnumNotFoundException getEnumNotFoundException(String enumName) {
+        return new EnumNotFoundException((enumName + " enum could not be found!"));
+    }
+
+    public static AlreadyExistsException getAlreadyExistsException(String name) {
+        return new AlreadyExistsException(name + " already exists");
+    }
+
+    public static AlreadyExistsException getAlreadyExistsException(String name, String param) {
+        return new AlreadyExistsException(name + " already exists with : " + param);
+    }
+
+    public static UserAlreadyExistsException getUserAlreadyExistsException(String name) {
+        return new UserAlreadyExistsException(name + " already exists");
+    }
+
+    public static UserAlreadyExistsException getUserAlreadyExistsException(String name, String param) {
+        return new UserAlreadyExistsException(name + " already exists with : " + param);
+    }
+
+    public static NullPasswordException getNullPasswordException() {
+        return new NullPasswordException("Password could not be empty!");
     }
 }
