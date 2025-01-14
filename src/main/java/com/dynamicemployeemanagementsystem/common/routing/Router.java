2d22c348669649b827b3f27b402e52e6add42653
Mohammad Nayem <sheikhmdnayem@gmail.com>
2025-01-14 17:03:04 +0600

    Added Department APIs' routing paths

--- src/main/java/com/dynamicemployeemanagementsystem/common/routing/Router.java
+++ src/main/java/com/dynamicemployeemanagementsystem/common/routing/Router.java
@@ -2,4 +2,12 @@ package com.dynamicemployeemanagementsystem.common.routing;
 
 public final class Router {
     private static final String ROOT = "/api/";
+
+    public static final class Department {
+        public static final String GET_DEPARTMENTS = ROOT + "departments";
+        public static final String GET_DEPARTMENT = ROOT + "departments/{id}";
+        public static final String CREATE_DEPARTMENT = ROOT + "departments";
+        public static final String UPDATE_DEPARTMENT = ROOT + "departments/{id}";
+        public static final String DELETE_DEPARTMENT = ROOT + "departments/{id}";
+    }
 }
