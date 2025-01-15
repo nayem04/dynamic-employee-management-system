169d75b10b5c6fb89abe05a0b32e338818f46139
Mohammad Nayem <sheikhmdnayem@gmail.com>
2025-01-15 12:09:42 +0600

    Added EmployeeDto class

---
+++ src/main/java/com/dynamicemployeemanagementsystem/domain/dtos/EmployeeDto.java
@@ -0,0 +1,39 @@
+package com.dynamicemployeemanagementsystem.domain.dtos;
+
+import com.dynamicemployeemanagementsystem.common.base.BaseDto;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.util.Date;
+
+@Getter
+@Setter
+@ToString
+public class EmployeeDto extends BaseDto {
+    @NotBlank(message = "Designation is required.")
+    @JsonProperty(value = "designation", required = true)
+    private String designation;
+
+    @NotNull(message = "Date Of Joining is required.")
+    @JsonProperty(value = "date_of_joining", required = true)
+    private Date dateOfJoining;
+
+    @NotNull(message = "Salary is required.")
+    @JsonProperty(value = "salary", required = true)
+    private double salary;
+
+    @JsonProperty(value = "additional_attributes")
+    private String additionalAttributes; // Store as JSONB
+
+    @NotNull(message = "Department Id is required.")
+    @JsonProperty(value = "department_id", required = true)
+    private Long departmentId;
+
+    @NotNull(message = "User Id is required.")
+    @JsonProperty(value = "user_id", required = true)
+    private Long userId;
+}
